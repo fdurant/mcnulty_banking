7d38267f57916b82817e51fd78ac9bdbe83246fc
fdurant <frederik.durant@pandora.be>
2015-02-18 14:06:26 -0500

    Replaced the d3.slider (now comes from https://github.com/sujeetsr/d3.slider)

--- d3/ProfitCurve.js
+++ d3/ProfitCurve.js
@@ -1,28 +1,40 @@
 var defaultAvgCostPerContact = 10;
-d3.select('#avgCostPerContactSlider')
-    .call(d3.slider()
-	  //	  	  .axis(true)
-	  .min(5)
-	  .max(50)
-	  .value(defaultAvgCostPerContact)
-	  .step(5)
-	  .on("slide", function(evt, value) {
-		  d3.select('#avgCostPerContactSliderText').text(value);
-		  recomputeAndRedraw();
-	      }));
+var costSlider = d3.slider()
+    .min(5)
+    .max(50)
+    .showRange(true)
+    .ticks(10)
+    .value(defaultAvgCostPerContact)
+    .stepValues([5,10,15,20,25,30,35,40,45,50]);
+
+var costCallbackFn = function(costSlider) {
+    //    console.log("selected cost = " + costSlider.value())
+    d3.select('#avgCostPerContactSliderText').text(costSlider.value());
+    recomputeAndRedraw()
+};
+
+var avgCostPerContactSlider = d3.select('#avgCostPerContactSlider').call(costSlider);
+costSlider.callback(costCallbackFn);
+
 
 var defaultAvgRevenuePerContact = 50;
-d3.select('#avgRevenuePerContactSlider')
-    .call(d3.slider()
-	  //	  	  .axis(true)
-	  .min(10)
-	  .max(100)
-	  .value(defaultAvgRevenuePerContact)
-	  .step(10)
-	  .on("slide", function(evt, value) {
-		  d3.select('#avgRevenuePerContactSliderText').text(value)
-		  recomputeAndRedraw();
-	      }));
+var revSlider = d3.slider()
+    .min(10)
+    .max(100)
+    .showRange(true)
+    .ticks(10)
+    .value(defaultAvgRevenuePerContact)
+    .stepValues([10,20,30,40,50,60,70,80,90,100]);
+
+var revCallbackFn = function(revSlider) {
+    //    console.log("selected revenue = " + revSlider.value())
+    d3.select('#avgRevenuePerContactSliderText').text(revSlider.value())
+    recomputeAndRedraw()
+};
+
+var avgRevenuePerContactSlider = d3.select('#avgRevenuePerContactSlider')
+    .call(revSlider);
+revSlider.callback(revCallbackFn);
 
 /* Borrowed here: http://stackoverflow.com/questions/8495687/split-array-into-chunks*/
 Array.prototype.chunk = function(chunkSize) {
@@ -144,6 +156,7 @@ d3.json("ProfitCurve.json", function(error, json) {
 
 	    var avgCostPerContactFromSlider = d3.select('#avgCostPerContactSliderText').text();
 	    var avgRevenuePerContactFromSlider = d3.select('#avgRevenuePerContactSliderText').text();
+
 	    var calculatedProfits = calculateProfits(sortedProbsAndTrueLabels,
 						     20, 
 						     1, 
@@ -154,7 +167,7 @@ d3.json("ProfitCurve.json", function(error, json) {
 	    var values = [];
 	
 	    for (var i = 0; i < calculatedProfits.length; i++) {
-		values.push({x: calculatedProfits[i][0], y: calculatedProfits[i][3], intervalProfit: calculatedProfits[i][6]});
+		values.push({x: Math.floor(calculatedProfits[i][0]), y: Math.floor(calculatedProfits[i][3]), intervalProfit: Math.floor(calculatedProfits[i][6])});
 	    }
 	
 	    mydata.push({ 'values': values, 'key': modelName, 'area': false});
@@ -165,7 +178,7 @@ d3.json("ProfitCurve.json", function(error, json) {
 		    
 		var chart = nv.models.lineChart()
 		    .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
-		    .useInteractiveGuideline(false)  //We want nice looking tooltips and a guideline!
+		    .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
 		    .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
 		    .showYAxis(true)        //Show the y-axis
 		    .showXAxis(true)        //Show the x-axis
@@ -218,13 +231,13 @@ d3.json("ProfitCurve.json", function(error, json) {
 		
 		chart.xAxis     //Chart x-axis settings
 		    .axisLabel('Percentage of test instances (decreasing by probability)')
-		    .tickFormat(d3.format('.1f'));
+		    .tickFormat(d3.format('d'));
 		
 		chart.yAxis     //Chart y-axis settings
-		    .axisLabel('Percentage of positive instances targeted')
-		    .tickFormat(d3.format('.1f'));
+		    .axisLabel('Profit (in USD)')
+		    .tickFormat(d3.format('d'));
 		
-		d3.select('svg')    //Select the <svg> element you want to render the chart in.   
+		d3.select('svg#profitcurve')    //Select the <svg> element you want to render the chart in.   
 		    .datum(mydata)         //Populate the <svg> element with chart data...
 		    .call(chart);          //Finally, render the chart!
 		
@@ -238,4 +251,4 @@ d3.json("ProfitCurve.json", function(error, json) {
 
 }
 
-recomputeAndRedraw();
+recomputeAndRedraw(10,50);
